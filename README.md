52a17095f9fcce344ce2f1ebb50793ab57f21b73
Matheus Torres <torrespmatheus@gmail.com>
2019-01-21 21:47:34 -0200

    Add README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Snorlajs
+A server creator for lazy developers who just want to write the SQL queries
+
+The code is just an PoC. 
+
+Fell free to clone and use this repo as a base and adapt to your needs.
--- databases/postgres.js
+++ databases/postgres.js
@@ -4,6 +4,6 @@ module.exports = {
     host: "localhost",
     port: 5432,
     username: "admin",
-    password: "postgrespass",
+    password: "postgrespass", //you should never commit the password, its just an example
     database: "postgres"
 }
